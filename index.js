54d87c96e0bf41bc7cc8a43b40ada6cebc805733
Sara Baronsky <sbaronsk@bowdoin.edu>
2018-06-28 07:14:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string) {if (string.toLowerCase() === string) {return "I can't hear you!"}
-}
+function sayHiToGrandma(string) {if (string.toLowerCase() === string) {return "I can't hear you!"} 
+{if (string.toUpperCase() === string) {return "YES INDEED!"}
